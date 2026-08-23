022766269bbc58ded6b6bae3acd9b5befe16ed97
Neiser Lozano Chavez <neyser.locha@gmail.com>
2026-08-22 23:32:02 -0500

    chore(v2): configure AngularFire and Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+  firebase: {
+    apiKey: 'AIzaSyA6eF4gkeDbHYTlUCPG3Pkwc0rUxJLVgL0',
+    authDomain: 'konteo-fiance.firebaseapp.com',
+    projectId: 'konteo-fiance',
+    storageBucket: 'konteo-fiance.firebasestorage.app',
+    messagingSenderId: '299692634553',
+    appId: '1:299692634553:web:acd7884ea8c6e94107c668',
+    measurementId: 'G-RK21E578ML',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,64 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withComponentInputBinding, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular';
+import 'zone.js';
 
-import { routes } from './app/app.routes';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withComponentInputBinding,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular';
+
+import {
+  provideFirebaseApp,
+  initializeApp,
+} from '@angular/fire/app';
+
+import {
+  provideAuth,
+  getAuth,
+} from '@angular/fire/auth';
+
+import {
+  provideFirestore,
+  getFirestore,
+} from '@angular/fire/firestore';
+
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy,
+    },
+
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
+
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withComponentInputBinding(),
+    ),
+
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebase),
+    ),
+
+    provideAuth(() =>
+      getAuth(),
+    ),
+
+    provideFirestore(() =>
+      getFirestore(),
+    ),
   ],
 });
