6846913f76aabbc39f5b7a898fa295f70a114a33
Neiser Lozano Chavez <neyser.locha@gmail.com>
2026-08-26 11:36:55 -0500

    feat(transactions): complete transaction creation navigation

--- src/app/layout/desktop-sidebar/desktop-sidebar.component.ts
+++ src/app/layout/desktop-sidebar/desktop-sidebar.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  signal,
+} from '@angular/core';
 
 import {
   RouterLink,
   RouterLinkActive,
 } from '@angular/router';
+
+import {
+  TransactionQuickMenuComponent,
+} from '../../shared/components/transaction-quick-menu/transaction-quick-menu.component';
 
 @Component({
   selector: 'app-desktop-sidebar',
@@ -13,6 +20,17 @@ import {
   imports: [
     RouterLink,
     RouterLinkActive,
+    TransactionQuickMenuComponent,
   ],
 })
-export class DesktopSidebarComponent {}
+export class DesktopSidebarComponent {
+  readonly quickMenuOpen = signal(false);
+
+  openQuickMenu(): void {
+    this.quickMenuOpen.set(true);
+  }
+
+  closeQuickMenu(): void {
+    this.quickMenuOpen.set(false);
+  }
+}
